8536cbb25f7636593d1b63e0bde69a0b9ad3252a
progfay <shun1856shun@gmail.com>
2019-03-14 16:38:16 +0900

    feat: generate json files of emoji snippet

---
+++ src/index.js
@@ -0,0 +1,31 @@
+var fs = require('fs')
+const uuidv4 = require('uuid/v4')
+const { ordered, lib } = require('emojilib')
+
+const generateEmojiSnippet = (name, char) => {
+  return new Promise((resolve, reject) => {
+    const uid = uuidv4().toUpperCase()
+    const entry = {
+      alfredsnippet: {
+        uid: uid,
+        keyword: `:${name}::`,
+        snippet: char,
+        name: `${char} :${name}:`,
+        dontautoexpand: false
+      }
+    }
+    fs.writeFile(`dist/${uid}.json`, JSON.stringify(entry, null, 2), (error, data) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+async function main () {
+  Promise.all(ordered.map(name => generateEmojiSnippet(name, lib[name].char)))
+}
+
+main()
